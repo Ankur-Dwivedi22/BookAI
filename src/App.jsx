3facab2ce076057f3e13e70b5569725c4a589a8d
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2024-08-16 15:56:12 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Cards from './components/Cards'
 import Card2 from './components/Card2'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
   
       <div className='bg-gradient-to-br from-[#1e0533] to-[#110a1f]  w-full '>
-        {/* <Cards /> */}
         <Card2 />
       </div>
   
